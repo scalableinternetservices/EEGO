e85a54052d0261e475d18b1c9400878943fe93df
darkherald <mrshi1992@gmail.com>
2015-11-20 13:54:19 -0800

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,9 @@ Adventure.create(location: "a", num_people: 8, num_day: 3, plan: "Hiking", user_
 Adventure.create(location: "b", num_people: 70, num_day: 3, plan: "Hiking", user_id: 2);
 Adventure.create(location: "c", num_people: 23, num_day: 3, plan: "Hiking", user_id: 1);
 Adventure.create(location: "a", num_people: 12, num_day: 3, plan: "Hiking", user_id: 1);
+
+
+User.create(email: "123@ucla.edu", password: "11111111", password_confirmation: "11111111");
+User.create(email: "234@ucla.edu", password: "22222222", password_confirmation: "22222222");
+User.create(email: "345@ucla.edu", password: "33333333", password_confirmation: "33333333");
+User.create(email: "456@ucla.edu", password: "44444444", password_confirmation: "44444444");
