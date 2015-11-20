286d28e2463706f577520d2776e222b1ffcf888e
darkherald <mrshi1992@gmail.com>
2015-11-19 23:03:30 -0800

    server side caching and fragment caching

--- app/helpers/adventures_helper.rb
+++ app/helpers/adventures_helper.rb
@@ -1,2 +1,8 @@
 module AdventuresHelper
+  def cache_key_for_adventure_row(adventure)
+    "adventure-#{adventure.id}-#{adventure.updated_at}"
+  end
+  def cache_key_for_adventure_table
+    "adventure-table-#{Adventure.maximum(:updated_at)}"
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,8 @@
 module GroupsHelper
+  def cache_key_for_group_row(group)
+    "group-#{group.id}-#{group.updated_at}"
+  end
+  def cache_key_for_group_table
+    "group-table-#{Group.maximum(:updated_at)}"
+  end
 end
