1aaa5981952e689ff99455e9f07b2fa6c3e1dec9
Robertxfj <Robertxfj@yahoo.com>
2015-11-24 15:57:51 -0800

    add data_cache avoid multiple lookup

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,3 +1,14 @@
 class Adventure < ActiveRecord::Base
   belongs_to :user
+  def self.fetch(location)
+    Rails.cache.fetch("adventure_#{location}") { Adventure.where("location LIKE ?","%#{location}%")}
+  end
+
+  def after_save
+    Rails.cache.write("adventure_#{location}", self)
+  end
+
+  def after_destroy
+    Rails.cache.delete("adventure_#{location}")
+  end
 end
