756cd8bf16f2898b8a758529f212588b18834d1f
darkherald <mrshi1992@gmail.com>
2015-12-01 14:52:27 -0800

    Search friend fragment caching

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -6,4 +6,12 @@ module FriendsHelper
   def is_pending_request? user
     !Friendship.where("user_id=#{current_user.id} AND friend_id=#{user.id} AND status=2").empty?
   end
+
+  def cache_key_for_friend_row(user)
+    "friend-#{user.id}-#{user.notifications.maximum(:updated_at)}"
+  end
+ # def cache_key_for_friend_table
+ #  "friend-table-#{Friendship.maximum(:updated_at).try(to_s)}"
+ # end
+
 end
