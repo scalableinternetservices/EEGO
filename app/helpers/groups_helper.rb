63b501c28071499696eb60c7dd982f6ee78d0650
darkherald <mrshi1992@gmail.com>
2015-12-01 23:18:58 -0800

    Fix bug for adventure_index

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,8 +1,8 @@
 module GroupsHelper
   def cache_key_for_group_row(group)
-    "group-#{group.id}-#{group.updated_at}"
+    "group-#{current_user.id}-#{group.id}-#{group.updated_at}"
   end
   def cache_key_for_group_table
-    "group-table-#{Group.maximum(:updated_at)}"
+    "group-table-#{current_user.id}-#{Group.maximum(:updated_at)}"
   end
 end
