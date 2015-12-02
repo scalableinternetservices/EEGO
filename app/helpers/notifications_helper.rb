ae2f9582680a3bffb90bda7bc05608d5e6bfdeeb
darkherald <mrshi1992@gmail.com>
2015-12-01 22:24:06 -0800

    Add notification fragment cache

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,4 +3,12 @@ module NotificationsHelper
     @friendship = Friendship.where("user_id=#{sender_user_id} AND friend_id=#{current_user.id}")
     return "/friends/#{@friendship[0].id}"
   end
+
+  def cache_key_for_notification_row(notification)
+    "notification-#{current_user.id}-#{notification.id}-#{notification.updated_at}"
+  end
+  def cache_key_for_group_table
+    "notification-table-#{current_user.id}-#{Notification.maximum(:updated_at)}"
+  end
+
 end
